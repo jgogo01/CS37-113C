53f63ec400c9b026bb10a101f3b70e437569c6e8
Natdanai Pinves <83464496+jgogo01@users.noreply.github.com>
2022-12-07 19:20:06 +0700

    Add : 02_Lecture Var

---
+++ Lecture/02_Var/02_Var.c
@@ -0,0 +1,35 @@
+/* Datatype
+Int
+
+int : Integer (4 Byte 32 Bit)
+char : Character (1 Byte 8 Bit)
+short : Short Integer (2 Byte 16 Bit)
+long : Long Integer (4 Byte 32 Bit)
+long long : Long Long Integer (8 Byte 64 Bit)
+
+float IEEE754 : Single Precision (4 Byte 32 Bit)
+duoble IEEE754 : Double Precision (8 Byte 64 Bit)
+unsigned : Unsigned Integer (4 Byte 32 Bit)
+signed : Signed Integer (4 Byte 32 Bit)
+
+void : ไม่มีประเภทข้อมูล Void (0 Byte 0 Bit)
+*/
+
+
+/* Name Var
+ประกอบด้วยตัวอักษร ตัวเลข และ _
+การตั้งชื่อตัวแปร ควรตั้งชื่อให้สื่อความหมาย
+snake_case เช่น hello_word
+camelCase เช่น helloWord
+CamelCase เช่น HelloWorld
+*/
+
+/* ค่าคงที่
+const คือ ค่าคงที่ ไม่สามารถเปลี่ยนแปลงค่าได้
+integer constant คือ ค่าคงที่ที่เป็นจำนวนเต็ม เช่น 1, 2, 3, 4, 5
+floating constant คือ ค่าคงที่ที่เป็นจำนวนทศนิยม เช่น 1.0, 2.0, 3.0, 4.0, 5.0
+character constant คือ ค่าคงที่ที่เป็นตัวอักษร เช่น 'a', 'b', 'c', 'd', 'e'
+string constant คือ ค่าคงที่ที่เป็นตัวอักษรหลายตัว เช่น "abc", "def", "ghi", "jkl", "mno"
+unsigned constant คือ ค่าคงที่ที่เป็นจำนวนเต็มบวก เช่น 1u, 2u, 3u, 4u, 5u
+unsigned long constant คือ ค่าคงที่ที่เป็นจำนวนเต็มบวก และมีขนาดใหญ่ เช่น 1ul, 2ul, 3ul, 4ul, 5ul
+*/
