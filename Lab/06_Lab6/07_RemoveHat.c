30264510a3ed7df0bd52bd217acf7dc10fa5426b
Natdanai Pinves <83464496+jgogo01@users.noreply.github.com>
2023-01-21 04:21:49 +0000

    Fix : Lab 06.7

--- Lab/06_Lab6/07_RemoveHat.c
+++ Lab/06_Lab6/07_RemoveHat.c
@@ -16,7 +16,8 @@ int main()
 	for (i = 1; i <= num; numbersPtr++, i++) {
 	  *numbersPtr = i;
 	}
-
+  
+  
 	// loop through count
 	for (i = 0; i < count; i++) {
 		scanf("%d", &target);
@@ -26,25 +27,26 @@ int main()
 	numbersPtr = &numbers[0];
 
 	// print elements in numbers using numberPtr
-	for (; *numbersPtr <= num; numbersPtr++) {
+	for (i = 0; i < num; numbersPtr++, i ++) {
 	  printf("%d ", *numbersPtr);
 	}
 	puts("");
 
+  
   return 0;
 }
 
 // remove target from array by pointer *array and append last position by 0
 void removeTarget(int *array, int size, int target){
-
   //Loop Find Target
-  for(int i = 0; i <= size; i++){
-    if(*(array+i) == target){
+  for(int i = 0; i < size; i++){ // 0
+    if(*(array+i) == target){ 
       //Loop Replace Next Index
       for(; i < size; i++){
         *(array+i) = *(array+(i+1));
       }
       *(array + (size - 1)) = 0;
+      break;
     }
   }
 }
